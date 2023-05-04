c4bfac179711abee7eb3ff7b04644ba7cff9f7eb
RilCritch <rilcritch@gmail.com>
2023-05-04 18:20:48 -0500

    Added script for favorite distro tube colorscripts

---
+++ Documents/useful_command_info/favorite_colorscripts.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/bash
+#
+# This script prints my favorite distrotube colorscripts for when I need to remember them
+#
+# Author: RilCritch
+#
+# Updated: April 9th, 2023
+
+seperator='\n---------------------------------------------------------------------------------------------------\n'
+
+fade='1 >> FADE'
+echo $fade
+colorscript -e 1
+echo -e $seperator
+
+square='2 >> SQUARE'
+echo $square
+colorscript -e 2
+echo -e $seperator
+
+blocks2='6 >> BLOCKS2'
+echo $blocks2
+colorscript -e 6
+echo -e $seperator
+
+hex='7 >> HEX'
+echo $hex
+colorscript -e 7
+echo -e $seperator
+
+awk='11 >> AWK-RGB-TEST'
+echo $awk
+colorscript -e 11
+echo -e $seperator
+
+print256='15 >> PRINT256'
+echo $print256
+colorscript -e 15
+echo -e $seperator
+
+bloks='18 >> BLOKS'
+echo $bloks
+colorscript -e 18
+echo -e $seperator
+
+arch='21 >> ARCH'
+echo $arch
+colorscript -e 21 
+echo -e $seperator
+
+blocks1='33 >> BLOCKS1'
+echo $blocks1
+colorscript -e 33 
+echo -e $seperator
+
+pinguco='35 >> PINGUCO'
+echo $pinguco
+colorscript -e 35 
+echo -e $seperator
+
+rails='38 >> RAILS'
+echo $rails
+colorscript -e 38 
+echo -e $seperator
+
+spectrum='40 >> SPECTRUM'
+echo $spectrum
+colorscript -e 40 
+echo -e $seperator
+
+bars='41 >> BARS'
+echo $bars
+colorscript -e 41
+echo -e $seperator
+
+crunchbangmini='43 >> CRUNCHBANG-MINI'
+echo $crunchbangmini
+colorscript -e 43
+echo -e $seperator
+
+panes='49 >> PANES'
+echo $panes
+colorscript -e 49
+echo -e $seperator
+
+zwaves='57 >> ZWAVES'
+echo $zwaves
+colorscript -e 57
+echo -e $seperator
